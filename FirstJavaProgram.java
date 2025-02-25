78c8cf45f8e609afc5871a36401d68fb8f0d3cd8
LoveleenGoyal <143306722+LoveleenGoyal@users.noreply.github.com>
2025-02-25 14:05:54 +0530

    Remote update FirstJavaProgram.java

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,5 @@
 public class FirstJavaProgram{
 	public static void main(String[] args){
-		System.out.println("Starting with Capgemini program");
+		System.out.println("Starting with Capgemini program Training..");
 	}
 }
